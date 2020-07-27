7f1258edfbdabef38148d4a4a5b7fcf8aa9a9659
rhpessoa <rhpessoa29@gmail.com>
2020-07-27 10:30:14 -0300

    Criação de transações modelo e listagem delas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,14 +13,45 @@ class ExpensesApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _transactions = [
+      Transaction(
+          id: 't1',
+          title: 'Novo Tênis de Corrida',
+          value: 310.76,
+          date: DateTime.now()),
+      Transaction(
+        id: 't2',
+        title: 'Conta de luz',
+        value: 211.30,
+        date: DateTime.now(),
+      )
+    ];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas Pessoais'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Versão inicial'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Despesas Pessoais'),
+          centerTitle: true,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Text(tr.title),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
